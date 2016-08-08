99ef4f0fc6ef70128c9b02481bffb1b676798127
llaull <laul@llovem.eu>
2016-08-08 09:55:38 +0200

    maj du script d'install ajout du app_dev.php dans le git

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,14 @@
-composer self-update
+#!/usr/bin/env bash
+
+git clone https://github.com/llaull/symfonyAlpha.git
+cd symfonyAlpha
+php composer self-update
 cd src
 git clone https://github.com/llaull/AppBundleX.git
 mv  -v AppBundleX/* AppBundle/
+rm -r AppBundleX
 cd ../
 mysql -e 'create database alpha;'
-composer install
+php bin/composer install
 php bin/console doctrine:schema:create
 php bin/console fos:user:create test test test --super-admin
